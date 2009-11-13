b8ae240518d6e2793503b57e46e2e6c049bd4b14
Toby Oxborrow <toby@oxborrow.net>
2009-11-13 16:48:13 +0800

    Script to test some functions are doing their job.

---
+++ servers/simple-web/test-dump.php
@@ -0,0 +1,18 @@
+<?php
+require_once('functions_backend.php');
+
+echo "<pre>Pool Cleaner(tm)\n\n";
+
+#$meta = get_http_meta('http://news.bbc.co.uk/1/hi/technology/8349923.stm');
+#$meta = get_http_meta('http://picasaweb.google.com/lh/photo/xr-C_wqHG8gssUZnzuv1Ow?feat=directlink');
+#$meta = get_http_meta('http://bugs.kde.org/show_bug.cgi?id=162326');
+#$meta = get_http_meta('http://lala.com/zPSy');
+#$meta = get_http_meta('http://tinyurl.com/yjbub48');
+echo "\n<hr>\n";
+#$meta = get_http_meta('http://ur.ly/3kSj');
+#echo "\n"; print_r($meta);
+#echo "\n<hr>\n";
+$meta = get_http_meta('http://thejesustv.com');
+echo "\n"; print_r($meta);
+exit;
+
